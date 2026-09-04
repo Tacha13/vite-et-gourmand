6982a1d1d9c9d43ddeeed54a7556ca171539a195
Natacha DENIEUL <tachadnl@hotmail.com>
2026-09-04 08:28:42 -0400

    CREATE TABLES menus, plats, allergenes, menu_plat, plat_allergene BDD

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
 USE vite_et_gourmand;
 
-CREATE TABLE comptes 
+CREATE TABLE IF NOT EXISTS comptes 
 (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR (250) UNIQUE NOT NULL,
@@ -15,4 +15,48 @@ commune VARCHAR(250),
 role VARCHAR (50) DEFAULT 'utilisateur',
 actif BOOLEAN DEFAULT 1,
 date_creation DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS menus 
+(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nomMenu VARCHAR (250) NOT NULL,
+description VARCHAR (250) NOT NULL,
+theme VARCHAR (250) NOT NULL,
+nbPersonneMin INTEGER NOT NULL,
+prix FLOAT NOT NULL,
+conditions VARCHAR (250) NOT NULL,
+regime VARCHAR (250) NOT NULL,
+stock INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS plats 
+(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nomPlat VARCHAR (250) NOT NULL,
+typePlat VARCHAR (250) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS allergenes 
+(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+allergene VARCHAR (250) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS menu_plat
+(
+menu_id INTEGER NOT NULL,
+plat_id INTEGER NOT NULL,
+PRIMARY KEY (menu_id, plat_id),
+Foreign Key (menu_id) REFERENCES menus(id),
+Foreign Key (plat_id) REFERENCES plats(id)
+);
+
+CREATE TABLE IF NOT EXISTS plat_allergene
+(
+plat_id INTEGER NOT NULL,
+allergene_id INTEGER NOT NULL,
+PRIMARY KEY (plat_id, allergene_id),
+Foreign Key (plat_id) REFERENCES plats(id),
+Foreign Key (allergene_id) REFERENCES allergenes(id)
 );
